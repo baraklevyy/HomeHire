8e3b4eb7d22f2f2121863d15806cbc2c489f08f1
Barak Levy <baraklevyy@gmail.com>
2021-08-29 20:28:49 +0300

    database.py documentation

--- database.py
+++ database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import Table, Column, String, Integer, MetaData, func, delete, update
 
 
+################################
+#      DATABASE FUNCTIONS      #
+################################
+
+
+
+# Function used to create a postgreSQL table.
 def listings_table(db):
     table = Table('listings', MetaData(db),
                   Column('renter', String, nullable=False),
@@ -11,26 +18,24 @@ def listings_table(db):
                   )
     return table
 
-
+# Function used to add new property listing to the table.
 def add_listing(listings_table, renter, streetAddress, description, imageUrl, transactionUrl):
     listings_table.insert().values(renter=renter, streetAddress=streetAddress, description=description,
                                 imageUrl=imageUrl, transactionUrl=transactionUrl).execute()
 
-"""
-delete row from the DataBase. Each renter can attain only one property so the deletion will be One-to-one function.
-"""
+# Function used to delete row from the DataBase. Each tenant can attain only one property so the deletion will be One-to-one relation.
 def delete_listing(listings_table, renter):
-    # listings_table.delete().returning(listings_table.c.row).\
-    # where(listings_table.c.renter==renter).execute()
-
     delete(listings_table).where(listings_table.c.renter == renter).execute()
 
+# Function used to update the Testnet (Kovan/Ropsten) transation URL.
 def update_transaction_url(listings_table, renter, transactionUrl):
     listings_table.update().where(renter=renter).values(transactionUrl=transactionUrl).execute().fetchall()
 
+# Function used to update the tenant address in the database table.
 def update_renter(listings_table, old_renter, new_renter):
     update(listings_table).where(listings_table.c.renter == old_renter).values(renter=new_renter).execute()
 
+# Getter function used to retreive the available listings.
 def get_listings(listings_table):
     select_statement = listings_table.select().execute()
     result_set = select_statement.fetchall()
@@ -40,7 +45,7 @@ def get_listings(listings_table):
         print(r)
     return listings
 
-
+# Getter function used to retrieve specific property given tenant address (one-to-one relation).
 def get_listing(listings_table, renter):
     select_statement = listings_table.select().where(listings_table.c.renter==renter).execute()
     listing = select_statement.fetchone()
