0f60facf4f2cdd9cfcb2eecf9b53e014e5ea65df
Barak Levy <baraklevyy@gmail.com>
2021-08-29 20:27:13 +0300

    Contract documentation

--- solidity/HomeHire.sol
+++ solidity/HomeHire.sol
@@ -1,10 +1,41 @@
+
+השיחה נפתחה. הודעה אחת שלא נקראה.
+
+דילוג לתוכן
+שימוש ב-Gmail עם קוראי מסך
+Meet
+Hangouts
+
+1 מתוך 8,282
+contract‏‏
+דואר נכנס
+
+Barak Levy <baraklevyy@gmail.com‏>
+קבצים מצורפים
+20:25 ‎(לפני 0 דקות)‎
+אני
+
+
+
+	בלי וירוסים. www.avast.com
+2 קבצים מצורפים
 pragma solidity ^0.6.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.0.0/contracts/math/SafeMath.sol";
 
 
+
+/*
+HomeHire smart-contract	
+*/
 contract HomeHire {
 
+	//safe-math package usage
+    using SafeMath for uint256;
+
+	//Contract deployer address
 	address public deployer;
 
+	//Property data-structure
     struct Home {
         address renter;
         address owner;
@@ -18,30 +49,27 @@ contract HomeHire {
         
     }
 
+	// Mapping used to track relationship between users and approved Landlords.
 	mapping (address => bool) public Landlords;
+
+	// Mapping used to track relationship between tenant to the exclussive property he rents. 
 	mapping (address => Home) public Homes;
 
-	event HomeRentChanged(
-	    address renter,
-	    uint256 newHomeRent
-	    );
-
-	event RenterChanged(
-	    address oldRenter,
-	    address newRenter
-	    );
-
+	//Only the deployer of the contract can add approved Landlords.
 	constructor() public {
         deployer = msg.sender;
     }
 
+	//Function used to add Landlord - Only the contract deployer can perfrom this action.
 	function addLandlord(address _landlord) public {
 	    require(msg.sender == deployer, "Only admin can call this function.");
 	    Landlords[_landlord] = true;
 	}
 
-	function addHome(uint256 _homeRent, uint256 _monthsToPay, address _renter, string memory _dateOfStart, string memory _dateOfEnd, string _elevator, string _garage, uint256 _roomNum) public {
+	//Function used to add home for rental. Each property is tenant-exclussive and only approved landlord can add home.
+	function addHome(uint256 _homeRent, uint256 _monthsToPay, address _renter, string memory _dateOfStart, string memory _dateOfEnd, string memory _elevator, string memory _garage, uint256 _roomNum) public {
 	    require(Landlords[msg.sender] == true, "Only approved landlords can list homes.");
+	    require(Homes[_renter].renter != _renter, "Renter can hire only one property.");
 	    Homes[_renter] = Home({
 	        renter: _renter,
 	        owner: msg.sender,
@@ -55,7 +83,9 @@ contract HomeHire {
 	    });
 	}
 
+	//Function used to update the property tenant. Each property is tenant-exclussive and only approved landlord can update home.
 	function changeRenter(address _oldRenter, address _newRenter, string memory _dateOfStart, string memory _dateOfEnd, uint256 _monthsToPay) public {
+	    require(Homes[_oldRenter].renter == _oldRenter, "Renter not have a home.");
 	    require(Homes[_oldRenter].owner == msg.sender, "Only the owner of this property can change the renter.");
 	    Homes[_oldRenter].monthsToPay = _monthsToPay;
 	    Homes[_oldRenter].dateOfStart = _dateOfStart;
@@ -63,18 +93,24 @@ contract HomeHire {
 	    Homes[_oldRenter].renter = _newRenter;
 	    Homes[_newRenter] = Homes[_oldRenter];
 	    delete Homes[_oldRenter];
-
-	    emit RenterChanged(_oldRenter, _newRenter);
 	}
 
+	//Function used to update the property rent-price. Each property is tenant-exclussive and only approved landlord can update rent-price.
 	function changeHomeRent(address _renter, uint256 _newHomeRent) public {
+	    require(Homes[_renter].renter == _renter, "Renter not have a home.");
 	    require(Homes[_renter].owner == msg.sender, "Only the owner of this property can update the home rent.");
 	    Homes[_renter].homeRent = _newHomeRent;
+	}
+	
+	//Function used to delete property. Only the contract deployer can delete a property. Used to enforce unprotected tenancy.
+	function deleteHome(address _renter) public {
+	    require(Homes[_renter].renter == _renter, "Renter not have a home.");
+	    require(msg.sender == deployer, "Only admin can call this function.");
+	    delete Homes[_renter];
 
-	    emit HomeRentChanged(_renter, _newHomeRent);
 	}
 
-
+	//Function used to pay a rent by the tenent to the appropriate landlord.
     function payRent(address payable _to) public payable {
         require(Landlords[_to] == true, "Rent must be sent to an approved landlord.");
         require(Homes[msg.sender].owner == _to, "Rent must be paid to the owner of the home.");
@@ -82,13 +118,15 @@ contract HomeHire {
         require(Homes[msg.sender].monthsToPay > 0, "Months to pay need to be more then 0");
 
         _to.transfer(msg.value);
-        Homes[msg.sender].monthsToPay -= 1;
+        Homes[msg.sender].monthsToPay = Homes[msg.sender].monthsToPay.sub(1);
 
     }
 
-    
+    //Getter function used to retrieve a property given tenant address(one-to-one relation)
     function getHome(address _renter) public view returns(address, address, uint256, uint256, uint256, string memory, string memory, string memory, string memory) {
         Home storage home = Homes[_renter];
         return (home.renter, home.owner, home.homeRent, home.roomNum, home.monthsToPay, home.dateOfStart, home.dateOfEnd, home.garage, home.elevator);
     }
 }
+contract.txt
+מציג את contract.txt.
